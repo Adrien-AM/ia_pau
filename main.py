777f2974b13684d52c7c2db3ed8961998b8457c6
JEAUFFROY Paul <paul.jeauffroy@enseirb-matmeca.fr>
2022-01-29 19:14:29 +0100

    script csv

---
+++ main.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+test_file = "../Claims_FRzip/reduced_claims_fr.txt"
+class_file = "../Classifications_A1-EP14-EP35zip/sort_classification.txt"
+
+
+data_frame_init = pd.read_csv(test_file, sep='\t')
+data_frame_class = pd.read_csv(class_file, sep='\t')
+data_frame = pd.DataFrame(
+    {
+        "claim-id" : [],
+        "claim-text" : [],
+        "class" : []
+    }
+)
+
+file = open(test_file, 'r')
+row = file.readline()
+
+cpt = 1
+
+while row:
+    row = row.split("\t")
+    if (row[1] is None or (row[0] is None)):
+        row = file.readline()
+        print("error ligne " + str(cpt))
+        pass
+    root = ET.fromstring("<root>" + row[2] + "</root>")
+    claim_class = []
+    class_fileO = open(class_file, "r")
+    row_class = class_fileO.readline()
+    find = False
+    while row_class:
+        row_class = row_class.split("\t")
+        if row_class[1] == str(row[0]) and row_class[2] == row[1]:
+            claim_class.append(row_class[4][:-1])
+            find = True
+        if find and (row_class[1] != str(row[0]) or row_class[2] != row[1]):
+            break
+        row_class = class_fileO.readline()
+    class_fileO.close()
+
+    for elem in root.iter('claim'):
+        claim_id = elem.attrib.get("id")
+        claim_text = ""
+        for mots in elem.find('claim-text').itertext():
+            claim_text += mots
+        if claim_id is None:
+            print("!!!!!!!!!!!!!!!!!!!!!!!!")
+            pass
+        tmp_data_frame = pd.DataFrame(
+            {
+        "claim-id" : [str(row[0]) + row[1] + claim_id],
+        "claim-text" : [claim_text],
+        "class" : [claim_class]
+            }
+        )
+        data_frame = pd.concat([data_frame, tmp_data_frame])
+    if (cpt % 1000 == 0):
+        data_frame.to_csv("test2.csv", sep="\t", mode="a")
+        data_frame = pd.DataFrame(
+            {
+                "claim-id": [],
+                "claim-text": [],
+                "class": []
+            }
+        )
+    row = file.readline()
+    print(cpt)
+    cpt += 1
+file.close()
+data_frame.to_csv("test.csv", sep="\t", mode="a")
+
+
+
