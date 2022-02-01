02f1c87c7556605e3e2091822d59f1cd4e045dd5
AGUILA--MULTNER Adrien <aaguilamultn@bordeaux-inp.fr>
2022-02-01 23:05:08 +0100

    shell script for data formatting

--- csv_to_fasttext.sh
+++ csv_to_fasttext.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
-then
-    echo "Usage : ./csv_to_fasttext.sh csvfile fasttext_formatted_file"
-    exit 1
-fi
+((TIME_PER_LINE=12)) # percent
 
-cat $1 | \
-    tail -n +2 | \
-    cut -d$'\t' -f 2- | \
-    sed	-e "s/.\+\t\(.\+\)\t\(.\+\)$/\2\t\1/" | \
-    sed -e "s/'\(...\).*-.\+',/__label__\1/g" | \
-    sed -e "s/'\(...\).*-.\+'\]/__label__\1/g" | \
-    sed -e "s/\[//g" > $2
+if [ $# -ne 3 ]
+then
+    echo "Usage : ./format_data.sh data_file classes_file output_file."
+    exit 0
+fi
+tmpfile=$(mktemp)
+
+trap "rm -f $tmpfile" EXIT
+
+echo -n "Creating temporary file from claims ..."
+
+# File for an easier format : ID LANG TEXT , separator is tab
+cat $1 | sed -e "/<claim num=/d" -e "s|EP\t\([0-9]*\s..\)\t....-..-..\t\(..\)\tCLAIM\t<claim id=\".*\"\snum=\"[0-9]*\">|\1\t\2\t|g" -e "s|</*claim-text>||g" -e "s|</claim>||g" > $tmpfile
+
+echo "" > $3
+
+echo -e "\tDone.\nNow writing to output. This is gonna take some time."
+
+# Compute estimated time. TIME_PER_LINE is arbitrary and will vary depending on computers.
+size=$(wc -l $tmpfile | cut -d' ' -f 1)
+((total=size*$TIME_PER_LINE / 100))
+((sec=total%60))
+((min=(total%3600)/60))
+((hour=total/3600))
+
+echo 'Estimated time : '$hour'h '$min'm '$sec's.'  
+
+# Associating ID with classes and writing it to output file
+while IFS= read -r line
+do
+    id=$(echo $line | cut -d' ' -f 1)
+    text=$(echo $line | cut -d' ' -f 4- | sed -e "s|<.*>||g" -e "s|([0-9]*)||g" -e "s/\r//g")
+    classes=$(grep $id $2 | cut -d$'\t' -f 5 | sed -e "s/\(...\).\+/\1/g" -e "s/[\r\n]//g" | sort | uniq)
+    for c in $classes
+    do
+	echo -n "__label__$c " >> $3
+    done
+    echo -e "\t$text" >> $3
+done < $tmpfile
 
 
